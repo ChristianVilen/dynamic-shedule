8101630b8a3dd52189c5b7672d2d04627d280091
Christian Vilen <christian.vilen@siili.com>
2022-10-31 19:21:31 +0200

    create script to add week data to db

---
+++ backend/seedData/seedWeeks.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Week"("weekNumber", "updatedAt") VALUES (1, now());
+INSERT INTO "Week"("weekNumber", "updatedAt") VALUES (2, now());
+INSERT INTO "Week"("weekNumber", "updatedAt") VALUES (3, now());
+INSERT INTO "Week"("weekNumber", "updatedAt") VALUES (4, now());
