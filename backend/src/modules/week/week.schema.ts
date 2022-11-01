0ea117dcd9ba6777b7c19a14892c51b064062583
Christian Vilen <christian.vilen@siili.com>
2022-11-01 17:22:32 +0200

    create week schema

---
+++ backend/src/modules/week/week.schema.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
+import { taskResponseSchema } from "../task/task.schema";
+
+const weekInput = {
+  weekNumber: z.number({
+    required_error: "weekNumber required",
+    invalid_type_error: "weekNumber type must be number",
+  }),
+  tasks: z.array(taskResponseSchema),
+};
+
+const weekGenerated = {
+  id: z.number(),
+  updatedAt: z.string(),
+  createdAt: z.string(),
+  deletedAt: z.string(),
+};
+
+const creatWeekSchema = z.object(weekInput);
+
+const weekResponseSchema = z.object({
+  ...weekInput,
+  ...weekGenerated,
+});
+
+const weeksResponseSchema = z.array(weekResponseSchema);
+
+export type CreateWeekInput = z.infer<typeof creatWeekSchema>;
+
+export const { schemas: weekSchemas, $ref } = buildJsonSchemas(
+  {
+    creatWeekSchema,
+    weekResponseSchema,
+    weeksResponseSchema,
+  },
+  { $id: "weekSchema" }
+);
