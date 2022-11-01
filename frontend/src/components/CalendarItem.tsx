29ff360d98b99bd2c1b2d8512a4ae35c755c85af
Christian Vilen <christian.vilen@siili.com>
2022-11-01 22:26:11 +0200

    add button which will mark task as done

--- frontend/src/components/CalendarItem.tsx
+++ frontend/src/components/CalendarItem.tsx
@@ -8,6 +8,10 @@ export const CalendarItem = ({ item, currentDateTime }: { item: ScheduleItem; cu
     return <div className={styles.item} />
   }
 
+  const onClickHandler = (id: number) => {
+    console.log(id)
+  }
+
   const hasCompletedTask = item.task && item.completed
   const isActive = currentDateTime.day === item.date.day
   const futureTask = item.date.day >= currentDateTime.day && item.hasOwnProperty('completed')
@@ -17,8 +21,18 @@ export const CalendarItem = ({ item, currentDateTime }: { item: ScheduleItem; cu
       <h2 className={`${styles.h2} ${hasCompletedTask ? styles.taskDay : ''} ${isActive ? styles.activeText : ''}`}>
         {item.date.day}
       </h2>
-      {hasCompletedTask ||
-        (futureTask && <h3 className={`${styles.h3} ${isActive ? styles.activeText : ''}`}>{item.task}</h3>)}
+      {hasCompletedTask ? (
+        <h3 className={styles.h3}>Completed!</h3>
+      ) : (
+        futureTask && (
+          <>
+            <h3 className={`${styles.h3} ${isActive ? styles.activeText : ''}`}>{item.task}</h3>
+            <button className={styles.btn} onClick={() => onClickHandler(item.id!)}>
+              completed?
+            </button>
+          </>
+        )
+      )}
     </div>
   )
 }
