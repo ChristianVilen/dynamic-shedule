69410c986ece812eaff66ccad9e980972dfd21b8
Christian Vilen <christian.vilen@siili.com>
2022-11-01 22:24:18 +0200

    modify types for getting data form backend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,16 +1,42 @@
 import { DateTime } from 'luxon'
 
 export type ScheduleItem = {
+  id: number | null
   date: DateTime | null
   task: string
   day: number
   completed?: boolean
 }
 
-export type ProgramWeek = Record<string, week[]>
+export type ProgramWeek = {
+  id: number
+  week: number
+  tasks: Task[]
+}
 
-type week = {
+type Task = {
+  id: number
   weekday: string
   title: string
   completed: boolean
+}
+
+type TaskIn = {
+  weekday: string
+  title: string
+  completed: boolean
+  weekId: number
+  id: number
+  updatedAt: Date
+  createdAt: Date
+  deletedAt: Date
+}
+
+export type WeekIn = {
+  weekNumber: number
+  tasks: TaskIn[]
+  id: number
+  updatedAt: Date
+  createdAt: Date
+  deletedAt: Date
 }
